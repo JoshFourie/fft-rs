30da1d303b5216ca80e9e0e2d82249c6590280b9
Josh Fourie <josh.fourie97@gmail.com>
2019-02-01 14:29:47 +1100

    impl Mul for PointWise struct, but will deprecate as it is in error.

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,7 @@
-use std::ops::{Add, Mul, Sub};
+use std::ops::{Add, Mul, Neg};
+
+// TODO pointwise_with_degree for multiplication
+
 
 #[derive(Debug, PartialEq, Clone)]
 pub struct PointWise<P> { points: Vec<Points<P>> }
@@ -31,11 +34,47 @@ where
             self.points
                 .into_iter()
                 .zip(rhs.points.into_iter())
-                .map(|(a, b)| {
-                    (a.degree, a.y + b.y)
-                })
+                .map( |(a, b)| (a.degree, a.y + b.y) )
                 .collect::<Vec<_>>()
         )
+    }
+}       
+
+impl<P> Neg for PointWise<P> 
+where 
+    P: Neg<Output=P>
+{
+    type Output=Self;
+    fn neg(self) -> Self {
+        Self::from( 
+            self.points
+                .into_iter()
+                .map( |i| (-i.degree, -i.y ) )
+                .collect::<Vec<_>>()
+        )
+    }
+}
+
+impl<P> Mul<Self> for PointWise<P> 
+where 
+    P: Mul<P, Output=P>
+    + Neg<Output=P>
+    + Clone,
+{
+    type Output=Self;
+    fn mul(self, rhs: Self) -> Self {
+        let n = self
+            .clone()
+            .points
+            .into_iter()
+            .zip(rhs.points.clone().into_iter())
+            .map( |(a, b)| (a.degree, a.y * b.y) );
+        let n_2 = self
+            .points
+            .into_iter()
+            .zip(rhs.points.into_iter())
+            .map( |(a, b)| (-a.degree, ( -a.y * -b.y ) ) );
+        Self::from( n_2.rev().chain(n).collect::<Vec<_>>() )
     }
 }
 
@@ -45,10 +84,21 @@ mod test {
     
     #[test]
     fn pointwise_addition() {
-        let Ax = PointWise::from( vec![ (0, 1), (1, 0), (2, 5), (3, 22) ] );
-        let Bx = PointWise::from( vec![ (0, 1), (1, 3), (2, 13), (3, 37) ] );
-        let Cx = PointWise::from( vec![ (0, 2), (1, 3), (2, 18), (3, 59) ] );
+        let Ax = PointWise::from( vec![ (0,1), (1,0), (2,5), (3,22) ]);
+        let Bx = PointWise::from( vec![ (0,1), (1,3), (2,13), (3,37) ]);
+        let Cx = PointWise::from( vec![ (0,2), (1,3), (2,18), (3,59) ]);
         assert_eq!(true, Ax.clone() + Bx == Cx);
         assert_eq!(false, Ax.clone() + Ax.clone() == Cx)
+    }
+
+    #[test]
+    fn pointwise_multiplication() {
+        let Ax = PointWise::from( vec![ (0,1), (1,0), (2,5), (3,22) ]);
+        let Bx = PointWise::from( vec![ (0,1), (1,3), (2,13), (3,37) ]);
+        let Cx = PointWise::from( vec![ (-3,340), (-2,9), (-1,2), (0,1), (1,0), (2,65), (3,814) ]);
+        println!("
+        {:?}
+        
+        {:?}", Ax * Bx, Cx);
     }
 }
